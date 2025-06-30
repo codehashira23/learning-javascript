805869502bb1c5fcca51f4fc08c8cd5bb12f8818
codehashira23 <divyanshjaiswal314@gmail.com>
2025-06-30 20:57:25 +0530

    objects

--- 2.js
+++ 2.js
@@ -3,8 +3,11 @@
 const person ={
     name:"Divyansh" ,
     age:21,
-    hobbies : ["football", "sleeping ","coding "]
+    hobbies : ["football", "sleeping ","coding "]// array bhi add ho sakta hai 
 };
 console.log(person.name);//accessing name which is the person object property  
 console.log(person.age); //to access properties we can use dot operator 
 
+// how to add key value pairs 
+person.gender = "male"; 
+console.log(person); //we cam see new key value pair added 
