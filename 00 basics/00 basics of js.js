6c8d105fb1e603db3b2c9bad941fdeedd14851ca
codehashira23 <divyanshjaiswal314@gmail.com>
2025-07-02 19:45:59 +0530

    3.2 done

--- 00 basics/00 basics of js.js
+++ 00 basics/00 basics of js.js
@@ -2,4 +2,9 @@
 // example tic tak toe game score , positions , resets  etc needs to be stored 
 // point : (code of softare = program) ---runs on --> RAM (lets 16gb)
 // program in running state becomes a process  --- task manager shows all process , in mac we have activity monitor   
-//   
+//   words vs keywords in a programming language 
+// keywords are reserved words , their meaning cannot be altered or repurposed 
+// let , for ,const ,if ,else , return etc...
+// to store values inside a program we have variables 
+// variables are like bucket in the memory that stores value
+// variables have names 
