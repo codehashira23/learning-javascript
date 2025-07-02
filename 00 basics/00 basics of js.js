48da671d32b6ae08633d806145652c72e54bf149
codehashira23 <divyanshjaiswal314@gmail.com>
2025-07-02 19:40:42 +0530

    covering basics  of js  from sanket singh lambda 3.0

---
+++ 00 basics/00 basics of js.js
@@ -0,0 +1,5 @@
+// every software needs to store some info 
+// example tic tak toe game score , positions , resets  etc needs to be stored 
+// point : (code of softare = program) ---runs on --> RAM (lets 16gb)
+// program in running state becomes a process  --- task manager shows all process , in mac we have activity monitor   
+//   
